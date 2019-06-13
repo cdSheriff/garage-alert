aa65b96075296e290a711ae873dbf35d3b5de81b
Colin Dess <pcolind@gmail.com>
2019-06-12 19:20:46 -0700

    adding mega
    played with the mega, did some keypad and lcd stuff

---
+++ display.py
@@ -0,0 +1,9 @@
+# Author: Colin Dess
+# Date: 5/28/2019
+# Python code to run on raspberry pi
+# collect all the radio sensors
+# display data in some cool way
+# maybe I should connect it to the web server?
+# Python because it has been a while,
+# and plays nicely with a pi
+
