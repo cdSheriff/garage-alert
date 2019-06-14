18710dcf04d718c19c4c035a22f4431ea815edbe
Colin Dess <pcolind@gmail.com>
2019-06-14 00:23:12 -0700

    Update keypad_to_LCD.ino

--- keypad_to_LCD/keypad_to_LCD.ino
+++ keypad_to_LCD/keypad_to_LCD.ino
@@ -48,13 +48,13 @@ const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
 //define the cymbols on the buttons of the keypads
 char hexaKeys[ROWS][COLS] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
 };
-byte rowPins[ROWS] = {A7, A6, A5, A4}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {A3, A2, A1, A0}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {A11, A12, A13, A14}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {A8, A9, A10}; //connect to the column pinouts of the keypad
 
 //initialize an instance of class NewKeypad
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
